e3126e5969c471b2cad80ea343a50b0bc203b133
Owinmowe <Owinmowe@gmail.com>
2022-05-29 23:00:16 -0300

    Add simple spawn manager editor class for seeing an overview of all the spawn in the scene

--- Assets/SimpleSpawnSystem/Scripts/Core/Editor/SimpleSpawnManagerEditor.cs
+++ Assets/SimpleSpawnSystem/Scripts/Core/Editor/SimpleSpawnManagerEditor.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UIElements;
 
 #if UNITY_EDITOR
 
 namespace SimpleSpawnSystem.Core
 {
 
+    [CustomEditor(typeof(SimpleSpawnManager))]
     public class SimpleSpawnManagerEditor : Editor
     {
 
@@ -25,7 +27,37 @@ namespace SimpleSpawnSystem.Core
 
         public override void OnInspectorGUI()
         {
+
             DrawDefaultInspector();
+
+            if (Application.isPlaying)
+            {
+
+                var script = (SimpleSpawnManager)target;
+
+                GUIStyle titleStyle = new GUIStyle();
+                titleStyle.fontSize = 20;
+                titleStyle.normal.textColor = Color.white;
+
+                GUIStyle spawnsStyle = new GUIStyle();
+                spawnsStyle.fontSize = 10;
+
+                GUILayout.Space(20);
+                GUILayout.Label("Current Spawns", titleStyle);
+                GUILayout.Space(20);
+                for (int i = 0; i < script.GetSpawnsCount; i++)
+                {
+                    var spawn = script.GetSpawn(i);
+                    spawnsStyle.normal.textColor = spawn.Data.SpawnColor;
+                    GUILayout.Label("Name: " + spawn.name, spawnsStyle);
+                    GUILayout.Label("Order Type: " + spawn.Data.OrderType.ToString(), spawnsStyle);
+                    GUILayout.Label("Area Type: " + spawn.Data.AreaType.ToString(), spawnsStyle);
+                    GUILayout.Label("Timer Type: " + spawn.Timer.CurrentTimerType.ToString(), spawnsStyle);
+
+                    GUILayout.Space(20);
+                }
+            }
+
         }
 
         #endregion
